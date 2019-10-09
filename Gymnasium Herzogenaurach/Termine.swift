6385c449f35f7076c8cfda930a06c4de9daef4ab
Philipp Dormann <ssspammail111@gmail.com>
2019-10-10 00:34:52 +0200

    #5 , closes #4 , closes #3 , closes #1

--- Gymnasium Herzogenaurach/Termine.swift
+++ Gymnasium Herzogenaurach/Termine.swift
@@ -6,5 +6,24 @@ class Termine: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         webView.load(URLRequest(url: URL(string: "https://gymh.philippdormann.de/termine/?platform=ios")!))
+        
+        guard let url = URL(string: "https://gymh.philippdormann.de/termine/api.php?out") else {return}
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let dataResponse = data,
+                error == nil else {
+                    print(error?.localizedDescription ?? "Response Error")
+                    return }
+            do{
+                let jsonResponse = try JSONSerialization.jsonObject(with:
+                    dataResponse, options: [])
+                print(jsonResponse) //Response result
+                
+                //TODO: parse json, display natively
+                
+            } catch let parsingError {
+                print("Error", parsingError)
+            }
+        }
+        task.resume()
     }
 }
